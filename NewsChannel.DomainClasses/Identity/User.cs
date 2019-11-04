9e671f8547ca1dcdd9b90ce08e3183d737d4bdde
iman solouki <imansolouki@outlook.com>
2019-11-04 13:42:15 +0330

    adding bio

--- NewsChannel.DomainClasses/Identity/User.cs
+++ NewsChannel.DomainClasses/Identity/User.cs
@@ -3,22 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using NewsChannel.DomainClasses.Business;
- 
+
 namespace NewsChannel.DomainClasses.Identity
 {
-    public class User:IdentityUser<int>
+    public class User : IdentityUser<int>
     {
-     public string FirstName { get; set; }   
-     public string LastName { get; set; }   
-     public DateTime? BirthDate { get; set; }   
-     public string Image { get; set; }   
-     public DateTime? RegisterDateTime { get; set; }   
-     public bool IsActive { get; set; }   
-     public GenderType Gender { get; set; } 
-     public virtual ICollection<News> News{get;set;}
-     public virtual ICollection<BookMark> BookMarks { get; set; }
-     public virtual ICollection<UserRole> Roles { get; set; }
-     public virtual ICollection<UserClaim> Claims { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string Image { get; set; }
+        public string Bio { get; set; }
+        public DateTime? RegisterDateTime { get; set; }
+        public bool IsActive { get; set; }
+        public GenderType Gender { get; set; }
+        public virtual ICollection<News> News { get; set; }
+        public virtual ICollection<BookMark> BookMarks { get; set; }
+        public virtual ICollection<UserRole> Roles { get; set; }
+        public virtual ICollection<UserClaim> Claims { get; set; }
     }
 
     public enum GenderType
